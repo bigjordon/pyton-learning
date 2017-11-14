83f7fafe17eaf09369504ef9261352e70e38f691
bigjordon <bigjordon@163.com>
2017-11-14 09:58:32 +0800

    add map use lambda

--- function_learn/lambda.py
+++ function_learn/lambda.py
@@ -24,3 +24,28 @@ B = [{"name": "aa", "age":24}, {"name": "bb", "age":26},\
 print(B)
 B.sort(key=lambda x:x["age"]) # error age4 can not find
 print(B)
+
+
+map( lambda x: x*x, [y for y in range(10)] )
+#class map(object)
+#|  map(func, *iterables) --> map object
+#|  
+#|  Make an iterator that computes the function using arguments from
+#|  each of the iterables.  Stops when the shortest iterable is exhausted.
+#|  
+#|  Methods defined here:
+#|  
+#|  __getattribute__(self, name, /)
+#|      Return getattr(self, name).
+#|  
+#|  __iter__(self, /)
+#|      Implement iter(self).
+#|  
+#|  __new__(*args, **kwargs) from builtins.type
+#|      Create and return a new object.  See help(type) for accurate signature.
+#|  
+#|  __next__(self, /)
+#|      Implement next(self).
+#|  
+#|  __reduce__(...)
+#|      Return state information for pickling.
